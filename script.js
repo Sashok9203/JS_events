c5ce4eeb83f46eb623049e4fd3df095012fd23e0
Sashok9203 <kovalchuk.olex@gmail.com>
2023-12-16 16:09:14 +0200

    Update

--- script.js
+++ script.js
@@ -5,23 +5,21 @@ const findElement =  document.querySelector('#find');
 const btype = document.querySelector('#btype');
 const findDataList = document.querySelector(`#findDataList`);
 const form = document.forms.creationForm;
-const year = form.elements.year;
+const date = form.elements.date;
 const brand = form.elements.brand;
 const model = form.elements.model;
 const color = form.elements.color;
-
-const findTableList =[];
+const findTableList = [];
 
 findElement.addEventListener("input", findUpadate);
 
 class Car {
     static initial = 1;
-
-    constructor(brand, model, color,year,bodyType) {
+    constructor(brand, model, color,date,bodyType) {
         this.brand = brand;
         this.model = model;
         this.color = color;
-        this.year = year;
+        this.date = new Date(date);
         this.bodyType = bodyType;
         this.id = Car.initial++;
     }
@@ -30,7 +28,7 @@ class Car {
     {
         return `<tr>
         <td>${this.id}</td>
-        <td>${this.year}</td>
+        <td>${this.date.getFullYear()}</td>
         <td>${this.brand}</td>
         <td>${this.model}</td>
         <td>${this.color}</td>
@@ -39,10 +37,10 @@ class Car {
     }
 }
 
-const cars = [new Car("Audi", "A8","Black",2008,"Sedan"),
-              new Car("Mercedes-Benz", "E-Class","Red", 2007,"Hatchback"),
-              new Car("Volkswagen", "Passat","Black",2016,"Coupe"),
-              new Car("BMW", "X5","Red",2023,"Crossover")];
+const cars = [new Car("Audi", "A8","Black","2008-12-1","Sedan"),
+              new Car("Mercedes-Benz", "E-Class","Red", "2007-1-1","Hatchback"),
+              new Car("Volkswagen", "Passat","Black","2016-3-4","Coupe"),
+              new Car("BMW", "X5","Red","2023-5-5","Crossover")];
 
 updateTable(cars);
 
@@ -87,18 +85,18 @@ function findUpadate() {
 addBtn.onclick = (event) => {
     
     event.preventDefault();
-    if(!brand.value ||!model.value || !color.value || !year.value || year.value < 0)
+    if(!brand.value ||!model.value || !color.value || !date.value || date.value < 0)
     {
         alert("Fill in all fields");
         return;
     }
-    const car = new Car(brand.value, model.value, color.value,year.value,btype.value);
+    const car = new Car(brand.value, model.value, color.value,date.value,btype.value);
     cars.push(car);
     tableBody.innerHTML += car.getHtml();
     brand.value = '';
     model.value = '';
     color.value = '';
-    year.value = '';
+    date.value = '';
 }
 
 clearBtn.onclick = () => {
